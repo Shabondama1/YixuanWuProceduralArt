b21f79f9b4c7d7f25bf61208fc4d7d256aeb3aef
yixuan <you@example.com>
2020-02-04 11:34:24 -0500

    what is this

---
+++ Assets/MoveAnObject.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveAnObject : MonoBehaviour
+{
+    public Vector3 position;
+    public Vector3 rotation;
+    public Vector3 scale;
+
+    public float speed;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        Vector3 jiggle = new Vector3(
+            position.x += speed, 
+            position.y + Random.value, 
+            position.z + Random.value   );
+        this.transform.position = jiggle;
+        this.transform.localEulerAngles = rotation;
+        this.transform.localScale = scale;
+    }
+}
+
---
+++ Assets/script/RuleBasedInstructions01.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RuleBasedInstructions01 : MonoBehaviour
+{
+    public bool rebuild;
+    public string[] standPositionOnChessBoard;
+  
+    public string[] chessPieceColor;
+    public int minNumber;
+    public int maxNumber;
+    public string[] chessPositionOnChessBoard;
+
+    public string[] skittlesColor;
+    public int minNumber02;
+    public int maxNumber02;
+    public string[]  candyPositionOnChessBoard;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        if(rebuild){
+          
+            string output = "Put two Chess boxes, ";
+
+            string sPOCBoard = standPositionOnChessBoard[Random.Range(0,standPositionOnChessBoard.Length)];
+
+            output += sPOCBoard + " of the board.\nChoose ";
+
+            int howMany = Random.Range(minNumber, maxNumber);
+            string cPColor = chessPieceColor[Random.Range(0,chessPieceColor.Length)];
+
+            output += howMany + " " + cPColor + " Chesses, place them ";
+
+            string chessPOCBoard = chessPositionOnChessBoard[Random.Range(0,chessPositionOnChessBoard.Length)];
+          
+            output += chessPOCBoard + " of the board.\nAnd choose ";
+
+            howMany *= 2;
+            output += howMany + " " + cPColor + " Chesses, " + chessPOCBoard + " of the board.";
+
+            int Several = Random.Range(minNumber02,maxNumber02);
+            string sColor = skittlesColor[Random.Range(0,skittlesColor.Length)];
+            string candyPOCBoard = candyPositionOnChessBoard[Random.Range(0,candyPositionOnChessBoard.Length)];
+
+            output += "\nThen, get " + Several + " piece of skittles, put them " + candyPOCBoard + ".";
+
+            Debug.Log(output);
+
+            rebuild = false;
+        
+        }
+    }
+}
