fb0685bdaad7ce2ad2cce0c9da930d8f40e8c5b5
yixuan <you@example.com>
2020-03-10 05:42:28 -0400

    midtermmmmm

---
+++ Assets/ClassWork/week 6/circleMaker1.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(LineRenderer))]
+
+public class circleMaker1 : MonoBehaviour
+{
+    public int vertexCount = 40;
+    public float startWidth = 0.2f;
+    public float endWidth = 0.2f;
+    public float radius;
+    public bool circleFillscreen;
+
+    private LineRenderer lineRenderer;
+
+    void DrawPolygon(int vertexNumber, float radius, Vector3 centerPos, float startWidth, float endWidth)
+    {
+        lineRenderer.startWidth = startWidth;
+        lineRenderer.endWidth = endWidth;
+        lineRenderer.loop = true;
+        float angle = 2 * Mathf.PI / vertexNumber;
+        lineRenderer.positionCount = vertexNumber;
+
+        for (int i = 0; i < vertexNumber; i++)
+        {
+            Matrix4x4 rotationMatrix = new Matrix4x4(new Vector4(Mathf.Cos(angle * i), Mathf.Sin(angle * i), 0, 0),
+                                                    new Vector4(-1 * Mathf.Sin(angle * i), Mathf.Cos(angle * i), 0, 0),
+                                    new Vector4(0, 0, 1, 0),
+                                    new Vector4(0, 0, 0, 1));
+            Vector3 initialRelativePosition = new Vector3(0, radius, 0);
+            lineRenderer.SetPosition(i, centerPos + rotationMatrix.MultiplyPoint(initialRelativePosition));
+
+        }
+    }
+
+}
+
+
---
+++ Assets/ClassWork/week 6/trapezoidMaker.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class trapezoidMaker : MonoBehaviour
+{
+    private LineRenderer lineRenderer;
+
+    void Start()
+    {
+        lineRenderer = GetComponent<LineRenderer>();
+        Vector3[] positions = new Vector3[4] { new Vector3(-0.5f, 0.9f, 0), new Vector3(-0.9f, -0.9f, 0), new Vector3(0.9f, -0.9f, 0), new Vector3(0.5f, 0.9f, 0)};
+        DrawTriangle(positions, 0.02f, 0.02f);
+    }
+
+    void DrawTriangle(Vector3[] vertexPositions, float startWidth, float endWidth)
+    {
+        lineRenderer.startWidth = startWidth;
+        lineRenderer.endWidth = endWidth;
+        lineRenderer.loop = true;
+        lineRenderer.positionCount = 4;
+        lineRenderer.SetPositions(vertexPositions);
+    }
+
+}
